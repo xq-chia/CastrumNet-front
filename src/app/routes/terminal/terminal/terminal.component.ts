71c8aacc7b00d2544798a2578414801fd499da91
Zachary Chia <xqchia614@outlook.com>
2023-11-02 15:33:26 +0800

    feat: Ctrl+C, DEL, ENTER view implementation

--- src/app/routes/terminal/terminal/terminal.component.ts
+++ src/app/routes/terminal/terminal/terminal.component.ts
@@ -8,8 +8,9 @@ import { TerminalService } from './terminal.service';
   templateUrl: './terminal.component.html'
 })
 export class TerminalTerminalComponent implements OnInit, AfterViewInit {
-  @ViewChild('term', { static: false }) child!: NgTerminal;
-  readonly prompt: string = '\n' + FunctionsUsingCSI.cursorColumn(1) + '$ ';
+  @ViewChild('term', { static: false }) term!: NgTerminal;
+  readonly prompt: string = '\r\n' + '$ ';
+  command: string = '';
 
   constructor(
     private http: _HttpClient,
@@ -19,18 +20,48 @@ export class TerminalTerminalComponent implements OnInit, AfterViewInit {
   ngOnInit(): void {}
 
   ngAfterViewInit(): void {
-    this.child.write('$ ');
-    this.socket.execute('test')
+    this.term.write('$ ')
 
-    this.child.onData().subscribe(input => {
+    this.term.onData().subscribe(input => {
       switch (input) {
+        // Enter (CR)
         case '\r':
+          if (this.command.length) {
+            this.runCommand(this.command);
+          }
           this.nextLine();
+
+          this.command = '';
+          break;
+        // Ctrl+C
+        case '\u0003':
+          this.term.write('^C');
+          this.nextLine();
+          
+          this.command = '';
+          break;
+        // Backspace (DEL)
+        case '\u007F':
+          if (this.command) {
+            this.term.write('\b \b');
+
+            this.command = this.command.slice(0, -1);
+          }
+          break;
+        default:
+          if (input >= String.fromCharCode(0x20) && input <= String.fromCharCode(0x7E) || input >= '\u00a0') {
+            this.command += input;
+            this.term.write(input);
+          }
       }
     });
   }
 
   private nextLine(): void {
-    this.child.write(this.prompt);
+    this.term.write(this.prompt);
+  }
+  
+  private runCommand(command: string): void {
+    this.socket.execute(command);
   }
 }
