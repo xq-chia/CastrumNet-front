2d40f55bb8cd3e929e5358f7c2a0a96f5900a21c
Zachary Chia <xqchia614@outlook.com>
2023-11-17 18:13:07 +0800

    feat: user-host relationship creation

---
+++ src/app/routes/host/host.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { _HttpClient } from '@delon/theme';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HostService {
+
+  constructor(private http: _HttpClient) { }
+
+  fetchAllHosts() {
+    return this.http.get<any[]>('/host');
+  }
+}
--- src/app/routes/user/create/create.component.ts
+++ src/app/routes/user/create/create.component.ts
@@ -3,6 +3,8 @@ import { SFSchema, SFUISchema } from '@delon/form';
 import { _HttpClient } from '@delon/theme';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { NzModalRef } from 'ng-zorro-antd/modal';
+import { HostService } from '../../host/host.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-user-create',
@@ -11,32 +13,6 @@ import { NzModalRef } from 'ng-zorro-antd/modal';
 export class UserCreateComponent implements OnInit {
   record: any = {};
   i: any;
-  // schema: SFSchema = {
-  //   properties: {
-  //     no: { type: 'string', title: '编号' },
-  //     owner: { type: 'string', title: '姓名', maxLength: 15 },
-  //     callNo: { type: 'number', title: '调用次数' },
-  //     href: { type: 'string', title: '链接', format: 'uri' },
-  //     description: { type: 'string', title: '描述', maxLength: 140 },
-  //   },
-  //   required: ['owner', 'callNo', 'href', 'description'],
-  // };
-  // ui: SFUISchema = {
-  //   '*': {
-  //     spanLabelFixed: 100,
-  //     grid: { span: 12 },
-  //   },
-  //   $no: {
-  //     widget: 'text'
-  //   },
-  //   $href: {
-  //     widget: 'string',
-  //   },
-  //   $description: {
-  //     widget: 'textarea',
-  //     grid: { span: 24 },
-  //   },
-  // };
   schema: SFSchema = {
     properties: {
       userId: { type: 'number', title: 'User Id' },
@@ -45,18 +21,34 @@ export class UserCreateComponent implements OnInit {
       firstName: { type: 'string', title: 'First Name' },
       lastName: { type: 'string', title: 'Last Name' },
       tenantId: { type: 'number', title: 'Tenant' },
+      hostIds: { type: 'string', title: 'Host' }
     },
     required: ['userId', 'username', 'password', 'firstName', 'lastName', 'tenantId'],
   };
-  ui: SFUISchema = {};
+  ui: SFUISchema = {
+    $hostIds: {
+      widget: 'select',
+      mode: 'multiple',
+      asyncData: () => this.fetchAllHosts().pipe(map(hosts => hosts.map(host => this.convertHostToSchema(host))))
+    }
+  };
 
   constructor(
     private modal: NzModalRef,
     private msgSrv: NzMessageService,
     public http: _HttpClient,
+    private hostService: HostService
   ) {}
 
   ngOnInit(): void {}
+
+  fetchAllHosts() {
+    return this.hostService.fetchAllHosts();
+  }
+
+  convertHostToSchema(host: any) {
+    return { label: `${host.host} | ${host.ipAddress}`, value: host.hostId };
+  }
 
   save(value: any): void {
     console.log(value)
