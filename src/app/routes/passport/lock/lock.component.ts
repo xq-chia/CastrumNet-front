340f95639163a7ee29fd4337b8a29655500d1bde
Zachary Chia <xqchia614@outlook.com>
2023-12-19 09:15:00 +0800

    feat: lock screen

--- src/app/routes/passport/lock/lock.component.ts
+++ src/app/routes/passport/lock/lock.component.ts
@@ -1,8 +1,12 @@
+import { HttpContext } from '@angular/common/http';
 import { Component, Inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { DA_SERVICE_TOKEN, ITokenService } from '@delon/auth';
-import { SettingsService, User } from '@delon/theme';
+import { StartupService } from '@core';
+import { ALLOW_ANONYMOUS, DA_SERVICE_TOKEN, ITokenService } from '@delon/auth';
+import { SettingsService, User, _HttpClient } from '@delon/theme';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { ConnectableObservable, catchError, of } from 'rxjs';
 
 @Component({
   selector: 'passport-lock',
@@ -21,19 +25,44 @@ export class UserLockComponent {
   constructor(
     @Inject(DA_SERVICE_TOKEN) private tokenService: ITokenService,
     private settings: SettingsService,
-    private router: Router
+    private router: Router,
+    private http: _HttpClient,
+    private msgSrv: NzMessageService,
+    private startupSrv: StartupService
   ) {}
 
   submit(): void {
     this.f.controls.password.markAsDirty();
     this.f.controls.password.updateValueAndValidity();
-    if (this.f.valid) {
-      console.log('Valid!');
-      console.log(this.f.value);
-      this.tokenService.set({
-        token: '123'
+    this.http.post(
+        '/auth/login',
+        {
+          username: this.user.email,
+          password: this.f.value.password
+        },
+        null,
+        {
+          context: new HttpContext().set(ALLOW_ANONYMOUS, true)
+        }
+    ).pipe(
+      catchError(err => {
+        this.msgSrv.error('Wrong Credential. Logging out');
+        return of(null);
+      })
+    ).subscribe(res => {
+      if (res == null) {
+        this.tokenService.clear()
+        this.router.navigate(['passport/login'])
+        return ;
+      }
+      this.tokenService.set(res.data.user);
+      this.startupSrv.load().subscribe(() => {
+        let url = this.tokenService.referrer!.url || '/';
+        if (url.includes('/passport')) {
+          url = '/';
+        }
+        this.router.navigateByUrl(url);
       });
-      this.router.navigate(['dashboard']);
-    }
+    })
   }
 }
