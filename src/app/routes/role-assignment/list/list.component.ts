abddd2864e1d0219f039bad54cdf0b92428a0203
Zachary Chia <xqchia614@outlook.com>
2023-12-23 09:35:04 +0800

    feat: segregation of duty

--- src/app/routes/role-assignment/list/list.component.ts
+++ src/app/routes/role-assignment/list/list.component.ts
@@ -1,19 +1,26 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
 import { STColumn, STComponent, STRes } from '@delon/abc/st';
 import { SFSchema } from '@delon/form';
 import { ModalHelper, _HttpClient } from '@delon/theme';
 import { RoleAssignmentEditComponent } from '../edit/edit.component';
 import { NzMessageService } from 'ng-zorro-antd/message';
+import { DA_SERVICE_TOKEN, ITokenService } from '@delon/auth';
 
 @Component({
   selector: 'app-role-assignment-list',
   templateUrl: './list.component.html',
 })
 export class RoleAssignmentListComponent implements OnInit {
+  userId: number = this.tokenSrv.get()!['id'];
   url = `/users`;
   res: STRes ={
     process: (_, res) => {
-      return res.data.users;
+      let users: any[];
+
+      users = res.data.users;
+      users = users.filter(user => user.userId != this.userId)
+
+      return users;
     }
   }
   searchSchema: SFSchema = {
@@ -49,7 +56,8 @@ export class RoleAssignmentListComponent implements OnInit {
   constructor(
     private http: _HttpClient, 
     private modal: ModalHelper,
-    private msgSrv: NzMessageService
+    private msgSrv: NzMessageService,
+    @Inject(DA_SERVICE_TOKEN) private tokenSrv: ITokenService,
   ) { }
 
   ngOnInit(): void { }
