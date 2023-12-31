7b7ea547b4ecd3c39414c390c14545ac6028171f
Zachary Chia <xqchia614@outlook.com>
2023-12-31 11:00:06 +0800

    feat: gmail suffix

--- src/app/global-config.module.ts
+++ src/app/global-config.module.ts
@@ -18,7 +18,9 @@ const alainConfig: AlainConfig = {
     license: `A59B099A586B3851E0F0D7FDBF37B603`,
     licenseA: `C94CEE276DB2187AE6B65D56B3FC2848`
   },
-  auth: { login_url: '/passport/login' }
+  auth: { login_url: '/passport/login' },
+  sf: { uiEmailSuffixes: ['gmail.com', 'outlook.com', 'yahoo.com', 'hotmail.com','mail.apu.edu.my', ] }
+
 };
 
 const alainModules: any[] = [AlainThemeModule.forRoot(), DelonACLModule.forRoot()];
